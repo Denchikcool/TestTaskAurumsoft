cb5cdb4490f6e14969bf462415fe0cca83902672
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-09 16:00:36 +0700

    Модель для валидации данных

---
+++ TestTask/Core/Models/ValidationError.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Models
+{
+    public class ValidationError
+    {
+        public int RowNumber { get; set; }
+        public string WellId { get; set; } = "";
+        public string ErrorMessage { get; set; } = "";
+    }
+}
