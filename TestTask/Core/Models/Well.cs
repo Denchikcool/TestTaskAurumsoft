465ba19cf05907539576c8be93bb38651a63f818
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-09 14:53:11 +0700

    Создание стартового скелета приложения (логика, WPF и тесты). Инициализация основных моделей

---
+++ TestTask/Core/Models/Well.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Core.Models
+{
+    public class Well
+    {
+        public string WellId { get; set; } = "";
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public List<Intervals> Intervals { get; set; } = new();
+    }
+}
