58c21865c27c95c718c07b9a060ff3dfa5ea7fc1
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-09 15:37:12 +0700

    Модель для хранения сводки о скважине в ходе расчетов

---
+++ TestTask/Core/Models/WellSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models
+{
+    public class WellSummary
+    {
+        public string WellId { get; set; } = "";
+        public int IntervalCount { get; set; }
+        public double TotalDepth { get; set; }
+        public double AvgPorosity { get; set; }
+        public string DominantRock { get; set; } = "";
+    }
+}
