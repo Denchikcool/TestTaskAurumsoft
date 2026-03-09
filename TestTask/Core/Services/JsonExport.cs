d6608e75b2a8206dc43f2fee4a75fd0564275402
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-09 17:17:20 +0700

    Класс для экспорта данных в JSON

---
+++ TestTask/Core/Services/JsonExport.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using Core.Models;
+
+namespace Core.Services
+{
+    public class JsonExport
+    {
+        public async Task ExportData(List<WellSummary> summaries, string path)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var json = JsonSerializer.Serialize(summaries, options);
+            await File.WriteAllTextAsync(path, json);
+        }
+    }
+}
