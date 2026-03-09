052e5672a7d11c467cb6133b0417aad51fd38b7f
Denchikcool <kuzmenok.den@yandex.ru>
2026-03-09 15:37:39 +0700

    Переименовал класс

---
+++ TestTask/Core/Models/Interval.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Models
+{
+    public class Interval
+    {
+        public double DepthFrom {  get; set; }
+        public double DepthTo { get; set; }
+        public string Rock { get; set; } = "";
+        public double Porosity { get; set; }
+
+        public double Length => DepthTo - DepthFrom; 
+    }
+}
